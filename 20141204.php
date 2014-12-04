63d74b6a693e7a42aa4545721ec6d62eb5bb2d03
JAM <tom83615@gmail.com>
2014-12-04 12:19:05 +0800

    Create 20141204.php
    base php file

---
+++ 20141204.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html lang="zh_TW">
+	<head>
+		<title><?php echo $title; ?></title>
+		<meta charset="utf-8" />
+		<!-- jQuery  -->
+	    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	</head>
+	<body background = <?php echo "'".$bg."'"; ?> >
+		<h1 class = ' text-center title'> OOXX </h1>
+		<div class="container">
+			<div class="col-md-12 row alert bg-base" >
+			<!--顯示背景框-->
+			</div>
+		</div>
+		<!-- Bootstrap -->
+		<link rel="stylesheet" type="text/css"  href=" bootstrap/css/bootstrap.min.css" />
+		<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+	</body>
+</html>
