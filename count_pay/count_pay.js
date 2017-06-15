1a7ac8c56bed6b76ae96c9188778bf9512a7cd36
謝瑜軒 <tom83615@gmail.com>
2017-06-15 23:13:25 +0800

    家聚計算器翻修

--- count_pay/count_pay.js
+++ count_pay/count_pay.js
@@ -1,21 +1,25 @@
 $(document).ready(function(){
     $(":input").blur(function(){
-        allFreshman = count($("#allFreshman").val());
-        allSenior = count($("#allSenior").val());
+        allFreshman = count($("#allFreshman").val()); 
+        seniorAttend = count($("#seniorAttend").val());
+        seniorAbsent = count($("#seniorAbsent").val());
+        allSenior = seniorAttend + seniorAbsent;
         dollar = Math.floor( count($("#dollar").val()) );
-        absentPay = Math.floor( (allFreshman * dollar) / allSenior );
-        attendPay = Math.floor( absentPay + dollar);
-        add1 = attendPay + absentPay;
-        add2 = add1 + absentPay;
-        total = dollar*(allFreshman+allSenior);
-        pay = attendPay*allSenior;
-        less =total-pay;
+
+
+        dollarSplit = dollar/(allFreshman + seniorAttend) ;  //單價
+        dollarTreat = dollarSplit * allFreshman / allSenior; //請客分擔
+        attendPay = Math.floor( dollarSplit + dollarTreat);
+        absentPay = Math.floor( dollarTreat ); 
+        add1 = attendPay + absentPay; // 幫一個人出
+        add2 = add1 + absentPay; // 幫兩個人出
+        pay = attendPay*seniorAttend + absentPay*seniorAbsent;
+        less =pay - dollar;
         if(allFreshman && allSenior && dollar){
             $("#attendPay").val(attendPay);
             $("#absentPay").val(absentPay);
             $("#add1").val(add1);
             $("#add2").val(add2);
-            $("#total").val(total);
             $("#pay").val(pay);
             $("#less").val(less);
         }
